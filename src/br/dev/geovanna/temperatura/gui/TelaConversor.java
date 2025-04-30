4fd88fee4281f915d6a6d02a5cea7a17deda6b02
GEOVANNA SILVA DE SOUSA <25132823@dsn127m1242373.cfp127aluno.local>
2025-04-30 17:00:06 -0300

    Tela iniciada

--- src/br/dev/geovanna/temperatura/gui/TelaConversor.java
+++ src/br/dev/geovanna/temperatura/gui/TelaConversor.java
@@ -1,5 +1,37 @@
 package br.dev.geovanna.temperatura.gui;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class TelaConversor {
+
+	private JTextField textCelsius;
+	private JLabel labelCelsius;
+	private JButton buttonKelvin;
+	private JButton buttonFahreinheit;
+	private JLabel labelResultado;
+	private JLabel labelMensagemErro;
+
+	public void criarTelaConversor() {
+		JFrame tela = new JFrame();
+		tela.setTitle("Conversor de Temperatura");
+		tela.setSize(400, 700);
+		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		tela.setLayout(null);
+		
+		//Componentes da tela
+		
+		textCelsius = new JTextField();
+		textCelsius.setBounds(0, 0, 0, 0);
+		
+		
+		buttonKelvin = new JButton();
+		buttonFahreinheit = new  JButton();
+		
+		
+
+	}
 
 }
--- src/br/dev/geovanna/temperatura/model/Temperatura.java
+++ src/br/dev/geovanna/temperatura/model/Temperatura.java
@@ -1,28 +1,24 @@
 package br.dev.geovanna.temperatura.model;
 
 public class Temperatura {
-	
+
 	private double celsius;
-	
-	public double getCelsius(){
+
+	public double getCelsius() {
 		return celsius;
-		
+
 	}
-	
+
 	public void setCelsius() {
-		
+
 	}
-	
-	public double converterParaKelvin() {
-		
-	}
-	
-	
+
 	public double converterParaFahreinheit() {
-		
+
 	}
-	
-	
-	
+
+	public double converterParaKelvin() {
+
+	}
 
 }
